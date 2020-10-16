c3e3383dff71d6b4567f5e940e3251e0e3f72b5c
Tatiana-N <tanuskanikolaeva@mail.ru>
2020-10-16 21:21:06 +0300

    5.2.2. Реализовать Store<T extends Base> [#388169]

---
+++ src/main/java/ru/job4j/generics/MemStore.java
@@ -0,0 +1,30 @@
+package ru.job4j.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemStore<T extends Base> implements Store<T> {
+
+  private final List<T> mem = new ArrayList<>();
+
+  @Override
+  public void add(T model) {
+mem.add(model);
+  }
+
+  @Override
+  public boolean replace(String id, T model) {
+    mem.set(Integer.parseInt(id), model);
+    return mem.get(Integer.parseInt(id)).equals(model);
+  }
+
+  @Override
+  public boolean delete(String id) {
+    return mem.remove(mem.get(Integer.parseInt(id)));
+  }
+
+  @Override
+  public T findById(String id) {
+    return mem.get(Integer.parseInt(id));
+  }
+}
