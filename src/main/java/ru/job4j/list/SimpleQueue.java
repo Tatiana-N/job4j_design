ae569ed0c08822e0aa325ba12c3a412f80992939
Tatiana-N <tanuskanikolaeva@mail.ru>
2020-10-20 09:51:33 +0300

    Попытка №2 5. Очередь на двух стеках [#388149]

--- src/main/java/ru/job4j/list/SimpleQueue.java
+++ src/main/java/ru/job4j/list/SimpleQueue.java
@@ -4,16 +4,25 @@ public class SimpleQueue<T> {
   SimpleStack<T> in = new SimpleStack<>();
   SimpleStack<T> out = new SimpleStack<>();
   private int sizeIn = 0;
+  private int sizeOut = 0;
 
   public T poll() {
+    sizeOut = sizeIn;
     while (sizeIn != 0) {
       out.push(in.pop());
       sizeIn--;
     }
+    sizeOut--;
     return out.pop();
+
   }
 
   public void push(T value) {
+    sizeIn = sizeOut != 0 ? sizeOut : sizeIn;
+    while (sizeOut != 0) {
+      in.push(out.pop());
+      sizeOut--;
+    }
     in.push(value);
     sizeIn++;
   }
