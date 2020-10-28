47db5bfcaa47024ce2c5e57e9c4c87aa31bf1572
Tatiana-N <tanuskanikolaeva@mail.ru>
2020-10-28 23:21:22 +0300

    8. Реализовать собственную структуру данных - HashMap [#388182] если при добавлении ключ уже есть, то возвращать false.

--- src/test/java/ru/job4j/map/SimpleMapTest.java
+++ src/test/java/ru/job4j/map/SimpleMapTest.java
@@ -7,33 +7,40 @@ import java.util.Iterator;
 public class SimpleMapTest {
   @Test
   public void testInsert() {
-    SimpleMap<Integer, String> map = new SimpleMap(1);
+    SimpleMap<Integer, String> map = new SimpleMap(0.75);
     map.insert(1, "Apple");
     map.insert(2, "Bpple");
     map.insert(3, "Cpple");
     map.insert(4, "Dpple");
-    System.out.println(map.insert(4, "Fpple"));
+    System.out.println(map.insert(10, "Fpple"));
     Iterator it = map.iterator();
-    System.out.println(it.next());
-    System.out.println(it.next());
-    System.out.println(it.next());
-    System.out.println(it.next());
-    System.out.println(it.next());
-   // System.out.println(it.next());
+    while (it.hasNext()) {
+      System.out.println(it.next());
+    }
   }
 
   @Test
   public void testGet() {
-    SimpleMap<Integer, String> map = new SimpleMap(1);
-    map.insert(17, "Apple");
-    map.insert(18, "Bpple");
-    map.insert(27, "Cpple");
-    map.insert(20, "Dpple");
-    map.insert(21, "Fpple");
-    System.out.println(map.get(27));
+    SimpleMap<Integer, String> map = new SimpleMap(0.75);
+    System.out.println(map.insert(17, "Apple"));
+    System.out.println(map.insert(18, "Bpple"));
+    System.out.println(map.insert(22, "Cpple"));
+    System.out.println(map.insert(20, "Dpple"));
+    System.out.println(map.insert(21, "Fpple"));
+    System.out.println(map.get(22));
   }
 
   @Test
   public void testDelete() {
+    SimpleMap<Integer, String> map = new SimpleMap(0.75);
+    map.insert(1, "Apple");
+    map.insert(2, "Bpple");
+    map.insert(3, "Cpple");
+    System.out.println(map.delete(2));
+    System.out.println(map.insert(10, "Fpple"));
+    Iterator it = map.iterator();
+    while (it.hasNext()) {
+      System.out.println(it.next());
+    }
   }
 }
