c0225c66fa005be29bc84c0ad36ebbcfb5c74a96
Tatiana-N <tanuskanikolaeva@mail.ru>
2020-11-05 05:35:39 +0300

    388140|2. Статистику по коллекции

--- src/main/java/ru/job4j/analize/Analize.java
+++ src/main/java/ru/job4j/analize/Analize.java
@@ -1,16 +1,35 @@
 package ru.job4j.analize;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Analize {
-
-
   public Info diff(List<User> previous, List<User> current) {
+Map<Integer, String> prev = new HashMap<>();
+    Map<Integer, String> cur = new HashMap<>();
+for (User user: previous){
+  prev.put(user.id, user.name);
+}
+    for (User user: current){
+      cur.put(user.id, user.name);
+    }
+Info info = new Info();
+    for (Integer id: cur.keySet()) {
+      if(prev.containsKey(id)){
+        if(!cur.get(id).equals(prev.get(id))){
+          info.changed++;
+        }
+      } else{info.added++;}
+
+    }
     return null;
 
   }
 
   public static class User {
-    int id;
-    String name;
+   private int id;
+   private String name;
 
     public User(int id, String name) {
       this.id = id;
@@ -22,9 +41,13 @@ public class Analize {
 
     int added;
     int changed;
-
     int deleted;
 
+    public Info() {
+      this.added = 0;
+      this.changed = 0;
+      this.deleted = 0;
+    }
   }
 
 }
