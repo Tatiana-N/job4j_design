8323581271a8a8c474d74a1e47b7d5f0da50dd9b
Tatiana-N <tanuskanikolaeva@mail.ru>
2020-11-06 00:57:49 +0300

    184458 | Почта

--- src/main/java/ru/job4j/exam/Application.java
+++ src/main/java/ru/job4j/exam/Application.java
@@ -5,34 +5,41 @@ import java.util.*;
 public class Application {
   public void start() {
     Scanner scanner = new Scanner(System.in);
-    Map<User, String> map = new HashMap<>();
-    //Set<User> userSet = new HashSet<>();
+    Map<String, String> map = new HashMap<>();
     System.out.println("Введите имя пользователя");
     String name = scanner.next();
     while (!name.equals("выход")) {
       System.out.println("Введите почту данного пользователя, если их несколько то введите их через запятую ");
       TreeSet<String> mails = new TreeSet<>(Arrays.asList(scanner.next().split(",")));
-      // userSet.add(user);
-      User user = new User(name, mails);
-      for (String s : mails) {
-       User alreadyExistUser =  map.put(s, user);
-       if(alreadyExistUser != null && alreadyExistUser != user){
-         alreadyExistUser.getMails().addAll(user.getMails());
-         map.put(s, alreadyExistUser);
-         user = alreadyExistUser;
-       }
+      for (String mail : mails) {
+        if (!map.containsKey(mail)) {
+          map.put(mail, name);
+        } else {
+          String nameTheSameUser = map.put(mail, name);
+          for (String m : map.keySet()) {
+            if (map.get(m).equals(nameTheSameUser)) {
+              map.put(m, name);
+            }
+          }
+        }
       }
       System.out.println("Введите имя следующего пользователя или \"выход\"");
       name = scanner.next();
     }
-    for (User user : map.values()) {
-      System.out.print(user.getName() + " : ");
-      for (String s : user.getMails()){
-        System.out.print(s + " ");
+    Set<String> users = new HashSet<>(map.values());
+    Map<String, Set<String>> result = new HashMap<>();
+    for (String user : users) {
+      for (String mail : map.keySet()) {
+        if (map.get(mail).equals(user)) {
+          if (!result.containsKey(user)) {
+            result.put(user, new HashSet<>());
+          }
+          result.get(user).add(mail);
+        }
       }
-      //   for (String s : user.getMails()) {
-      //      System.out.print(s + ",");
-      //   }
+    }
+      for (String n : result.keySet()) {
+        System.out.print(n + " " + result.get(n));
       System.out.println();
     }
   }
