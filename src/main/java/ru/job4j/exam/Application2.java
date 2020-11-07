94421bc98612f059cc60b633e546f3fb15c364ec
Tatiana-N <tanuskanikolaeva@mail.ru>
2020-11-07 20:36:35 +0300

    184458 | Почта| consolidation Mails Of Users in method

--- src/main/java/ru/job4j/exam/Application2.java
+++ src/main/java/ru/job4j/exam/Application2.java
@@ -4,26 +4,43 @@ import java.util.*;
 
 public class Application2 {
   public static void main(String[] args) {
+    Application2 application2 = new Application2();
+    Set<User> userSet = application2.inputAllUsers();
+    application2.consolidationMailsOfUsers(userSet);
+  }
+
+  public Set<User> inputAllUsers() {
+    Set<User> listOfUsers = new HashSet<>();
     Scanner scanner = new Scanner(System.in);
-    Map<String, String> mapKMailVUser = new HashMap<>();
-    Map<String, Set<String>> mapKUserVSetMails = new HashMap<>();
-    Set<String> theSameUsers = new HashSet<>();
     String name = scanner.next();
     while (!name.equals("exit")) {
       TreeSet<String> mails = new TreeSet<>(Arrays.asList(scanner.next().split(",")));
-      for (String mail : mails) {
-          theSameUsers.add(mapKMailVUser.put(mail, name));
-      }
-        mapKUserVSetMails.put(name, mails);
-        for (String n : theSameUsers) {
-          if (mapKUserVSetMails.containsKey(n)) {
-            mapKUserVSetMails.get(name).addAll(mapKUserVSetMails.get(n));
-            mapKUserVSetMails.remove(n);
-          }
-      }
+      User user = new User(name, mails);
+      listOfUsers.add(user);
       name = scanner.next();
     }
     scanner.close();
+    return listOfUsers;
+  }
+
+  public void consolidationMailsOfUsers(Set<User> set) {
+    Set<String> theSameUsers = new HashSet<>();
+    Map<String, String> mapKMailVUser = new HashMap<>();
+    Map<String, Set<String>> mapKUserVSetMails = new HashMap<>();
+    Iterator<User> it = set.iterator();
+    while (it.hasNext()) {
+      User user = it.next();
+      for (String mail : user.getMails()) {
+        theSameUsers.add(mapKMailVUser.put(mail, user.getName()));
+      }
+      mapKUserVSetMails.put(user.getName(), user.getMails());
+      for (String n : theSameUsers) {
+        if (mapKUserVSetMails.containsKey(n)) {
+          mapKUserVSetMails.get(user.getName()).addAll(mapKUserVSetMails.get(n));
+          mapKUserVSetMails.remove(n);
+        }
+      }
+    }
     for (String f : mapKUserVSetMails.keySet()) {
       System.out.print(f + " " + mapKUserVSetMails.get(f));
       System.out.println();
