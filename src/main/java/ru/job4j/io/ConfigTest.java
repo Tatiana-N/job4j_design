2790991c012c7834637e0a8ea487123229631e5f
Tatiana-N <tanuskanikolaeva@mail.ru>
2020-11-08 11:26:20 +0300

    388201 1. Читаем файл конфигурации

---
+++ src/main/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+  @Test
+  public void whenPairWithoutComment() {
+    String path = "./data/pair_without_comment.properties";
+    Config config = new Config(path);
+    config.load();
+    assertThat(
+      config.value("name"),
+      is("Tatiana Nikolaeva")
+    );
+  }
+
+  @Test
+  public void whenFile() {
+    String path = "C:/Tanusha/BOOKS/java/task/app.properties";
+    Config config = new Config(path);
+    config.load();
+    assertThat(
+      config.value("hibernate.connection.username"),
+      is("postgres")
+    );
+  }
+}
