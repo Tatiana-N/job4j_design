ff1e9dfd196501b20f33d1d528c03f3380bcc771
Tatiana-N <tanuskanikolaeva@mail.ru>
2020-11-29 01:32:23 +0300

    Архивировать проект [#388203] файлы затирают друг друга..

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,16 +1,19 @@
 package ru.job4j.io;
 
+import ru.job4j.search.src.s.Search;
+
 import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-  public void packFiles(List<File> sources, File target) {
-    for (File source : sources) {
-      source.toPath().
+  public void packFiles(List<Path> sources, File target) {
+    for (Path source : sources) {
+      packSingleFile(source.toFile(), target);
     }
-
   }
 
   public void packSingleFile(File source, File target) {
@@ -24,10 +27,22 @@ public class Zip {
     }
   }
 
-  public static void main(String[] args) {
-    new Zip().packSingleFile(
-      new File("./chapter_005/pom.xml"),
-      new File("./chapter_005/pom.zip")
-    );
+  public static void main(String[] args) throws IOException {
+    // new Zip().packSingleFile(
+    //    new File("./chapter_005/pom.xml"),
+    //     new File("./chapter_005/pom.zip")
+    //  );
+    ArgZip argZip = new ArgZip(args);
+    if (argZip.valid()) {
+      Path start = Paths.get(argZip.directory());
+      Zip zip = new Zip();
+      File fileOut = new File(argZip.output());
+      if (!fileOut.getAbsoluteFile().exists()) {
+        fileOut.createNewFile();
+      }
+      List<Path> ourlist = Search.search(start, "");
+      ourlist.removeAll(Search.search(start, argZip.exclude()));
+      zip.packFiles(ourlist, fileOut);
+    }
   }
 }
