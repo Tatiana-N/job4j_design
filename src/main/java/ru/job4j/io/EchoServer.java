81fd284565134cca606e503cbd1d70476bdba1db
Tatiana-N <tanuskanikolaeva@mail.ru>
2020-12-06 12:21:18 +0300

    Server answer you

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -12,8 +12,12 @@ public class EchoServer {
         try (OutputStream out = socket.getOutputStream();
              BufferedReader in = new BufferedReader(
                new InputStreamReader(socket.getInputStream()))) {
+          String answer = "";
           String str;
           while (!(str = in.readLine()).isEmpty()) {
+            if (str.contains("msg")) {
+              answer = str.substring(str.lastIndexOf("=") + 1, str.indexOf("HTTP") - 1);
+            }
             if (str.contains("msg=Bye ")) {
               out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
               server.close();
@@ -21,7 +25,10 @@ public class EchoServer {
             }
             System.out.println(str);
           }
-          out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+
+          out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+          out.write(answer.getBytes());
+          out.flush();
         }
       }
     }
