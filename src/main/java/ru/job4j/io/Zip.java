10fd232e9f1e3e9d9ae474d0155e38c01663aacc
Tatiana-N <tanuskanikolaeva@mail.ru>
2020-12-06 11:17:37 +0300

    388203 Архивировать проект с помощью SearchFiles

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,43 +1,66 @@
 package ru.job4j.io;
 
-import ru.job4j.search.src.s.Search;
+import ru.job4j.search.src.files.SearchFiles;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-  public void packFiles(List<Path> sources, File target) throws IOException {
+  private Path start;
+  private final File fileOut;
+  private final ArgZip argZip;
+
+  private void packFiles(List<Path> sources, File target) throws IOException {
     ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)));
     for (Path source : sources) {
       zip.putNextEntry(new ZipEntry(source.toString()));
-      BufferedInputStream out =  new BufferedInputStream(new FileInputStream(source.toFile()));
-        zip.write(out.readAllBytes());
+      BufferedInputStream out = new BufferedInputStream(new FileInputStream(source.toFile()));
+      zip.write(out.readAllBytes());
       out.close();
     }
     zip.close();
 
   }
 
-  public static void main(String[] args) throws IOException {
-    // new Zip().packSingleFile(
-    //    new File("./chapter_005/pom.xml"),
-    //     new File("./chapter_005/pom.zip")
-    //  );
-    ArgZip argZip = new ArgZip(args);
+  public Zip(String[] args) {
+    argZip = new ArgZip(args);
     if (argZip.valid()) {
-      Path start = Paths.get(argZip.directory());
-      Zip zip = new Zip();
-      File fileOut = new File(argZip.output());
-      if (!fileOut.getAbsoluteFile().exists()) {
+      start = Paths.get(argZip.directory());
+    }
+    fileOut = new File(argZip.output());
+    if (!fileOut.getAbsoluteFile().exists()) {
+      try {
         fileOut.createNewFile();
+      } catch (IOException e) {
+        e.printStackTrace();
       }
-      List<Path> ourlist = Search.search(start, "");
-      ourlist.removeAll(Search.search(start, argZip.exclude()));
-      zip.packFiles(ourlist, fileOut);
     }
   }
+
+  public void packingFiles() {
+    try {
+      packFiles(this.getList(start, path -> !path.toString().contains(argZip.exclude())), fileOut);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) {
+    Zip zip = new Zip(args);
+    zip.packingFiles();
+  }
+
+  public List<Path> getList(Path start, Predicate<Path> pred) throws IOException {
+    SearchFiles<Path> searchFiles = new SearchFiles<>(pred);
+    Files.walkFileTree(start, searchFiles);
+    return searchFiles.getPaths();
+  }
+
 }
+
