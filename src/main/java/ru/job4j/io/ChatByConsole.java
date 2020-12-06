149b00a4343d1f7be01bedd7539a64ba1f9b8ad8
Tatiana-N <tanuskanikolaeva@mail.ru>
2020-12-06 10:23:46 +0300

    ChatByConsole + using DispatchPattern

--- src/main/java/ru/job4j/io/ChatByConsole.java
+++ src/main/java/ru/job4j/io/ChatByConsole.java
@@ -3,32 +3,43 @@ package ru.job4j.io;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ChatByConsole {
-  private static final String OUT = "закончить";
-  private static final String STOP = "стоп";
-  private static final String CONTINUE = "продолжить";
-  private StringBuilder result;
   private ArgZip argZip;
-  private Boolean flagStopChat = true;
+  private ArrayList<String> chat;
+  private DispatchPattern dp;
+  long numOfLines = 0;
+  int rand = 0;
 
   public ChatByConsole(String[] args) {
-    result = new StringBuilder();
+    chat = new ArrayList<>(50); //список для записи чата
     String[] newArgs = Arrays.copyOf(args, args.length + 1);
-    newArgs[args.length] = "-e=";
+    newArgs[args.length] = "-e="; // костыль для использования уже написанного класса ArgZip
     this.argZip = new ArgZip(newArgs);
+    try (
+      Stream<String> lines = Files.lines(new File(argZip.directory()).toPath())
+    ) {
+      numOfLines = lines.count(); // посчитаем строки в файле ответов
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    dp = new DispatchPattern();
+    dp.init();
   }
 
   public void run() throws IOException {
+
     Scanner scanner = new Scanner(System.in);
     String answer = "";
     String userText = scanner.nextLine();
-    while (!userText.equals(OUT)) {
+    while (dp.check(userText)) {
       answer = this.returnAnswer(userText);
-      addString(userText);
+      this.addString(userText);
       if (!answer.isEmpty()) {
         System.out.println(answer);
         this.addString(answer);
@@ -45,25 +56,9 @@ public class ChatByConsole {
   }
 
   private String returnAnswer(String userText) {
-    if (userText.equals(STOP)) {
-      flagStopChat = false;
-    }
-    if (userText.equals(CONTINUE)) {
-      flagStopChat = true;
-    }
     String answer = "";
-    if (flagStopChat) {
-      int rand = 0;
-      try (
-        Stream<String> lines = Files.lines(new File(argZip.directory()).toPath())
-      ) {
-        long numOfLines = lines.count();
-        rand = (int) (Math.random() * numOfLines); // рандомный номер строки
-      } catch (FileNotFoundException e) {
-        e.printStackTrace();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
+    if (dp.getFlag()) {
+      rand = (int) (Math.random() * numOfLines); // рандомный номер строки
       try (BufferedReader bR = new BufferedReader(new FileReader(argZip.directory()))) {
         while (rand-- > 0) {
           answer = bR.readLine();
@@ -76,11 +71,11 @@ public class ChatByConsole {
   }
 
   private void addString(String elseString) {
-    this.result.append(elseString + "\n");
+    this.chat.add(elseString);
   }
 
   private String getAllDialog() {
-    return result.toString();
+    return chat.stream().map(p -> p + "\n").collect(Collectors.joining());
   }
 
   private boolean writeDialogInFile() throws IOException {
@@ -91,5 +86,4 @@ public class ChatByConsole {
     }
     return true;
   }
-
 }
