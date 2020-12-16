e2602323c45c679156b5e35376a6e2614d67af2a
Tatiana-N <tanuskanikolaeva@mail.ru>
2020-12-16 23:29:00 +0300

    388190 вывод Exception in log

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,11 +1,16 @@
 package ru.job4j.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-  public static void main(String[] args) throws IOException {
+  private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+  public static void main(String[] args) {
     DispatchPatternForServer dss = new DispatchPatternForServer();
     dss.init();
     try (ServerSocket server = new ServerSocket(9000)) {
@@ -33,6 +38,8 @@ public class EchoServer {
           out.flush();
         }
       }
+    } catch (IOException e) {
+      LOG.error("java.io.IOException – if an I/O error occurs when opening the socket.", e);
     }
   }
 }
--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -16,8 +16,14 @@ public class UsageLog4j {
     float weight = 75.5f;
     byte workingHoursInDay = 8;
     short sleepHoursInDay = 7;
-    char  gender = 'м';
+    char gender = 'м';
     LOG.debug("User info name : {}, gender : {}, height : {}, weight : {}, age : {}, dayOfLife : {}, isMarried : {}, workingHoursInDay : {}, sleepHoursInDay {}",
       name, gender, height, weight, age, dayOfLife, isMarried, workingHoursInDay, sleepHoursInDay);
-     }
+
+    try {
+      throw new Exception("Not supported code");
+    } catch (Exception e) {
+      LOG.error("Exception in log example", e);
+    }
+  }
 }
