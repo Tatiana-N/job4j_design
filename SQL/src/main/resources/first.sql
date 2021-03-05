bdd4ad9384d649e43c0e313f451a8b03b8313b7f
Tatiana-N <tanuskanikolaeva@mail.ru>
2021-03-05 19:54:25 +0300

    My first work with SQL, results save to file sql.sql

---
+++ SQL/src/main/resources/first.sql
@@ -0,0 +1,6 @@
+insert into people (name, passport, birth_day) values ('Tom', 43123434, '1990-01-01');
+select * from people;
+update people set name='Tanya', passport = 1234567;
+select * from people;
+delete from people;
+select * from people;
