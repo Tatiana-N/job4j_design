632d2eaef67dfd6aaf744ccafe4d6bd0ed927ba8
Tatiana-N <tanuskanikolaeva@mail.ru>
2021-03-10 21:02:41 +0300

    aggregate functions with having

--- SQL/src/main/java/functions/task/functions.sql
+++ SQL/src/main/java/functions/task/functions.sql
@@ -8,3 +8,7 @@ group by p.name;
 -- цена всех устройств человека
 select p.name as name , sum(d.price) as cost_of_devices  from people as p join devices_people as dp on dp.people_id = p.id join devices as d on d.id = dp.device_id
 group by p.name;
+-- цена всех устройств человека > 4000
+select p.name as name , avg(d.price) as cost_of_devices  from people as p join devices_people as dp on dp.people_id = p.id join devices as d on d.id = dp.device_id
+group by p.name
+having avg(d.price) > 4000;
